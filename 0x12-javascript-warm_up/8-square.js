7e7f77f6713d0e32d4018641e319277078853cd3
Faith Ozoanieke <45066966+FaithOzoanieke@users.noreply.github.com>
2023-03-15 17:08:05 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for(let i = 0; i < size; i++) {
+  for (let u = 0; u < size; u++) {
     let row = '';
-    for(let u = 0; u < size; u++) row += 'X';
-      console.log(row);
+    for (let f = 0; f < size; f++) row += 'X';
+    console.log(row);
   }
 }
