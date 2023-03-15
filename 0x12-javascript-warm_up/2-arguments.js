5eb507f9c06df6d095d07777a8000b73e3c245c4
FaithOzoanieke <faithozoanieke@gmail.com>
2023-03-15 01:52:06 -0700

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(process.argv.length === 2 ? 'No argument' : process.argv.length === 3 ? 'Argument found' ? 'Arguments found');
+if(process.argv.length === 2) {
+	console.log('No argument');
+} else if (process.argv.length === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
