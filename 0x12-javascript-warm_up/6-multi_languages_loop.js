e3ff955d39f54845340b7b9ee126f32fdbfaa742
FaithOzoanieke <faithozoanieke@gmail.com>
2023-03-15 03:49:00 -0700

    multi languages loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for(const allLines of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+for (const allLines of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(allLines);
 }
