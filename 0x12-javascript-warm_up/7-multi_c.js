5ed597b57549dac78954222af3e9eb53fad7f53a
Faith Ozoanieke <45066966+FaithOzoanieke@users.noreply.github.com>
2023-03-15 12:53:22 +0100

    multi C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,9 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   const x = Number(process.argv[2]);
-      for (i = 0; i < x; i++) {
-        console.log('C is fun');
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
   }
 }
