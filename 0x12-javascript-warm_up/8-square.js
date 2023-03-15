dd7e8011984c0c5397d220da6f1160fb4dd065b4
FaithOzoanieke <faithozoanieke@gmail.com>
2023-03-15 08:47:39 -0700

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for(i = 0; i < size; i++) {
+  for(let i = 0; i < size; i++) {
     let row = '';
-    for(u = 0; u < size; u++) row += 'X';
+    for(let u = 0; u < size; u++) row += 'X';
       console.log(row);
   }
 }
